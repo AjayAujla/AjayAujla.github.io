428f45ec54f48f8c23c2b9694ae893ed1119776e
Ajayveer Singh Aujla <ajay.aujla@hotmail.com>
2022-05-07 00:38:19 -0400

    Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey Yajaroo.
-        </p>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello Stock Market Helper.
         </p>
         <a
           className="App-link"
